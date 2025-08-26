82e196288a1df928b0ffc8dff8c7ed396e1ff40e
aayakovenko <aayakovenko@>
2025-08-26 10:38:28 +0300

    add geo icon + bit of layout

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,31 @@
-<script setup></script>
+<script setup>
+import axios from 'axios';
+import { ref, computed, onMounted } from 'vue';
+
+
+onMounted(() => {
+  axios.get('https://nlstar.com/ru/api/catalog3/v1/menutags/', {
+      params: {
+        'city_id': 1
+      }, 
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error('Ошибка при запросе:', error);
+    });
+  // Ключевые поля для использования:
+  // name - название категории
+  // text_color - цвет текста названия категории
+  // image - изображение для плитки категории
+  // slug - уникальный тэг категории, для запроса данных категории и формирования адреса страниц
+});
+
+</script>
 
 <template>
   <header class="header wrapper">
@@ -6,7 +33,12 @@
       <div class="current-city">Выбор города</div>
     </div>
   </header>
-  
+  <div class="wrapper">
+    <div class="catalog-categories container">
+      категории
+    </div>
+  </div>
+
 </template>
 
 <style>
@@ -14,16 +46,15 @@
 @import url('./assets/main.css');
 </style>
 <style scoped>
-.wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 .header {
   width: 100%;
   background-color: #FFFFFF;
   box-shadow: 0px 2px 4px 0px #2727271A;
   height: 72px;
 }
+.current-city {
+  font-family: 'FuturaPTBold', sans-serif;
+}
+
 </style>
 
