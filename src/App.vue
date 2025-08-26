5809ba386decd6831e25bb58cc1e1d1e5c5334d1
aayakovenko <aayakovenko@>
2025-08-26 09:54:05 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+  <header class="header wrapper">
+    <div class="container">
+      <div class="current-city">Выбор города</div>
+    </div>
+  </header>
+  
+</template>
+
+<style>
+@import url('./assets/reset.css');
+@import url('./assets/main.css');
+</style>
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.header {
+  width: 100%;
+  background-color: #FFFFFF;
+  box-shadow: 0px 2px 4px 0px #2727271A;
+  height: 72px;
+}
+</style>
+
